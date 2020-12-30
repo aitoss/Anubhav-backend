3d7a5e053389f9eb9694cefd1df9fa4240b5250d
Satya-here <satya.prakash9500@gmail.com>
2020-12-30 20:09:39 +0530

    approve through mail done

--- api/article/index.js
+++ api/article/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const {addArticle,getArticles,getArticle,getAllCompanies,getCompanyArticles} = require('./controller');
+const {addArticle,getArticles,getArticle,getAllCompanies,getCompanyArticles, authenticateArticle} = require('./controller');
 
 const router = express.Router();
 router.route('/').post(addArticle).get(getArticles);
 router.route('/getAllCompanies').get(getAllCompanies)
 router.route('/company/:companyName').get(getCompanyArticles)
+router.route('/verify/:encryptedString').get(authenticateArticle)
 router.route('/:articleId').get(getArticle)
 
 module.exports = router;
