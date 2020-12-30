73a7d5919e6d67ff63b5fc25f2a3dbf1a64f2ff3
AkshaySharma008 <akshaysharma30121999@gmail.com>
2020-12-30 11:17:21 +0530

    router change

--- api/article/index.js
+++ api/article/index.js
@@ -5,7 +5,7 @@ const {addArticle,getArticles,getArticle,getAllCompanies,getCompanyArticles} = r
 const router = express.Router();
 router.route('/').post(addArticle).get(getArticles);
 router.route('/getAllCompanies').get(getAllCompanies)
-router.route('/:companyName').get(getCompanyArticles)
+router.route('/company/:companyName').get(getCompanyArticles)
 router.route('/:articleId').get(getArticle)
 
 
