8790a46c36f35025780064ede32b39040f6b08fc
AkshaySharma008 <akshaysharma30121999@gmail.com>
2021-01-01 23:56:14 +0530

    search by tag

--- api/article/index.js
+++ api/article/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const {addArticle,getArticles,getArticle,getAllCompanies,getCompanyArticles, authenticateArticle} = require('./controller');
+const {addArticle,getArticles,getArticle,getAllCompanies,getCompanyArticles, authenticateArticle,getArticlesByTag} = require('./controller');
 
 const router = express.Router();
 router.route('/').post(addArticle).get(getArticles);
+router.route('/tags').post(getArticlesByTag)
 router.route('/getAllCompanies').get(getAllCompanies)
 router.route('/company/:companyName').get(getCompanyArticles)
 router.route('/verify/:encryptedString').get(authenticateArticle)
 router.route('/:articleId').get(getArticle)
+
 
 module.exports = router;
